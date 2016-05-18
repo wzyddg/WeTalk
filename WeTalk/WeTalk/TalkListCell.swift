259678eeb0d99841cf642645a5064c1406b8daff
wzyddg <haloilove@gmail.com>
2016-05-18 21:53:08 +0800

    talk list

---
+++ WeTalk/WeTalk/TalkListCell.swift
@@ -0,0 +1,24 @@
+//
+//  TalkListCell.swift
+//  WeTalk
+//
+//  Created by 王泽宇 on 16/5/18.
+//  Copyright © 2016年 iOSCourse. All rights reserved.
+//
+
+import UIKit
+
+class TalkListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
